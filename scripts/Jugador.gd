55aee8b49977b6e4b4f21528fab4327979d4e537
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2026-05-05 01:57:14 +0100

    feat(player): add idle and walking animations

--- scripts/Jugador.gd
+++ scripts/Jugador.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const VELOCIDAD = 150.0
+var ultima_direccion = "down"
 
 func _physics_process(delta):
 	var direccion = Vector2.ZERO
@@ -14,9 +15,24 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		direccion.y -= 1
 	
-	# Normalizar para que en diagonal no vaya más rápido
 	if direccion.length() > 0:
 		direccion = direccion.normalized()
 	
 	velocity = direccion * VELOCIDAD
 	move_and_slide()
+	_actualizar_animacion(direccion)
+
+func _actualizar_animacion(direccion: Vector2):
+	if direccion.x > 0:
+		ultima_direccion = "right"
+	elif direccion.x < 0:
+		ultima_direccion = "left"
+	elif direccion.y > 0:
+		ultima_direccion = "down"
+	elif direccion.y < 0:
+		ultima_direccion = "up"
+	
+	if direccion.length() > 0:
+		$Sprite.play("walk_" + ultima_direccion)
+	else:
+		$Sprite.play("idle_" + ultima_direccion)
