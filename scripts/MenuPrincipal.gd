3815b4653e80e254779916bc731b486dc204fff1
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2026-05-11 22:34:08 +0100

    feat(menu): add BGM loop to main menu

--- scripts/MenuPrincipal.gd
+++ scripts/MenuPrincipal.gd
@@ -2,6 +2,8 @@ extends Control
 
 func _ready():
 	$BotonJugar.pressed.connect(_on_boton_jugar_pressed)
+	$BGM.finished.connect(func(): $BGM.play())
+	$BGM.play()
 
 func _on_boton_jugar_pressed():
 	get_tree().change_scene_to_file("res://escenas/Mundo.tscn")
