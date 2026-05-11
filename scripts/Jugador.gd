b5d2ee3324583430c978e0d1704aca68830596f9
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2026-05-11 22:34:02 +0100

    feat(player): add immunity during barraquito, punch and flash reactions

--- scripts/Jugador.gd
+++ scripts/Jugador.gd
@@ -2,17 +2,22 @@ extends CharacterBody2D
 
 const VELOCIDAD_NORMAL = 80.0
 const VELOCIDAD_BOOST = 180.0
+
 var velocidad_actual = VELOCIDAD_NORMAL
 var ultima_direccion = "down"
 var barraquito_activo = false
 var bebiendo = false
+var inmune = false
 
-func _physics_process(delta):
+func _ready():
+	add_to_group("jugador")
+
+func _physics_process(_delta):
 	if bebiendo:
 		return
-		
+
 	var direccion = Vector2.ZERO
-	
+
 	if Input.is_action_pressed("ui_right"):
 		direccion.x += 1
 	if Input.is_action_pressed("ui_left"):
@@ -21,10 +26,10 @@ func _physics_process(delta):
 		direccion.y += 1
 	if Input.is_action_pressed("ui_up"):
 		direccion.y -= 1
-	
+
 	if direccion.length() > 0:
 		direccion = direccion.normalized()
-	
+
 	velocity = direccion * velocidad_actual
 	move_and_slide()
 	_actualizar_animacion(direccion)
@@ -32,7 +37,7 @@ func _physics_process(delta):
 func _actualizar_animacion(direccion: Vector2):
 	if bebiendo:
 		return
-		
+
 	if direccion.x > 0:
 		ultima_direccion = "right"
 	elif direccion.x < 0:
@@ -41,7 +46,7 @@ func _actualizar_animacion(direccion: Vector2):
 		ultima_direccion = "down"
 	elif direccion.y < 0:
 		ultima_direccion = "up"
-	
+
 	if direccion.length() > 0:
 		if barraquito_activo:
 			$Sprite.play("run_" + ultima_direccion)
@@ -53,13 +58,25 @@ func _actualizar_animacion(direccion: Vector2):
 func activar_barraquito():
 	bebiendo = true
 	$Sprite.play("drink")
-	# Esperar a que termine la animación de beber
 	await $Sprite.animation_finished
 	bebiendo = false
 	barraquito_activo = true
+	inmune = true
 	velocidad_actual = VELOCIDAD_BOOST
-	
-	# Barraquito dura 8 segundos
+	get_parent().activar_barraquito_efectos()
+
 	await get_tree().create_timer(8.0).timeout
 	barraquito_activo = false
+	inmune = false
 	velocidad_actual = VELOCIDAD_NORMAL
+	get_parent().desactivar_barraquito_efectos()
+
+func recibir_puñetazo():
+	if inmune:
+		return
+	get_parent().jugador_recibe_daño(100, "punch")
+
+func recibir_flash():
+	if inmune:
+		return
+	get_parent().activar_ceguera()
