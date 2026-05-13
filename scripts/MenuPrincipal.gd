bcc0a9ba922505cb8acb6775f83110fcfe59994d
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2026-05-13 10:55:42 +0100

    feat(menu): switch 'Jugar' label to white on hover

--- scripts/MenuPrincipal.gd
+++ scripts/MenuPrincipal.gd
@@ -1,9 +1,20 @@
 extends Control
 
+const COLOR_NORMAL := Color(0.23, 0.13202, 0.066700004, 1)
+const COLOR_HOVER := Color(1, 1, 1, 1)
+
 func _ready():
 	$BotonJugar.pressed.connect(_on_boton_jugar_pressed)
+	$BotonJugar.mouse_entered.connect(_on_jugar_hover_in)
+	$BotonJugar.mouse_exited.connect(_on_jugar_hover_out)
 	$BGM.finished.connect(func(): $BGM.play())
 	$BGM.play()
 
 func _on_boton_jugar_pressed():
 	get_tree().change_scene_to_file("res://escenas/Mundo.tscn")
+
+func _on_jugar_hover_in():
+	$BotonJugar/Texto.label_settings.font_color = COLOR_HOVER
+
+func _on_jugar_hover_out():
+	$BotonJugar/Texto.label_settings.font_color = COLOR_NORMAL
